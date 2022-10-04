63160ce32d5c5a3a668b9e8cd6384ad770013d32
Igor Mariano <igor-mariano@outlook.com>
2022-10-04 15:25:55 -0300

    Adicionando um telefone a lista de telefones

--- src/Consultorio.java
+++ src/Consultorio.java
@@ -1,6 +1,6 @@
 public class Consultorio extends Clinica {
     private String unidade;
-    private Telefone[] telefones;
+    private Telefone[1] telefones;
     private int numTels;
 
     public Consultorio(String unidade, Telefone[] telefones, int numTels, String nome, String cnpj) {
