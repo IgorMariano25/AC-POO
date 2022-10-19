31cb1960a0ab9876f38cafb68dc2b1fd913a8905
Igor Mariano <igor-mariano@outlook.com>
2022-10-19 10:06:41 -0300

    TODO arquivo Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 // import java.time.LocalDate;
 import java.util.Scanner;
 
+// Critérios de pontuçaõ de acordo com o professor Victor Machado
+  // --> TODO: Verificar se as variáveis e métodos foram bem nomeados (Uso de camelCase, nomes claros, etc...)
+  // --> TODO: Verificar se as classes e métodos foram bem nomeados (Uso de camelCase, nomes claros, etc...)
+  // --> TODO: Verificar se o espaçamento está correto (a + b ao invés de a+b)
+  // --> TODO: Instruções tiveram espaçamento correto (print("exemplo") ao invés de print("exemplo"))
+  // --> TODO: Linhas com comprimentos adequados
+  // --> TODO: Código está rodando quando é executado ?
+  // --> TODO: Código não apresenta redundância
+  // --> TODO: Armarzenar dados de input no Arry pacientes, Medicos Agenda, Consulta, Clinica
+  // --> TODO: Devover informaçõs de input
+  // --> TODO: Exibir mnsagem de erro, caso a informação buscada não exista
+  // --> TODO: Verificar o melhor tipo de Arry para ser aplicado
+  // --> TODO: Organizar exibição de mensagens para o usuário
+  // --> TODO: Sistema permite exibir Pacientes
+  // --> TODO: Sistema permite exibir Medicos
+  // --> TODO: Sistema permite exibir Agendas
+  // --> TODO: Sistema permite exibir Consultas
+  // --> TODO: Código de interface estár organizado entre Main e CadastraDados
+  // --> TODO: Classe Consultório foi impelmentada confrome o modelo UML
+  // --> TODO: Classe consultório foi implementada, sem acoplamentes ou redundâncias
+  // --> TODO: Representação da classe consultorio foi incluída corretamente no modelo UML
+  // --> TODO: Representação do método emitirNota() foi incluida corretamente no modelo UML, classe conslutório
+  // --> TODO: Método emitirNota() - gera cobrança de R$ 400
+  // --> TODO: Método emitirNota() - gera cobrança R$ 200 para a primeira consulta
+  // --> TODO: Método emitirNota() - gera cobrança R$ 100 para plano de saúde
+  // --> TODO: Método emitirNota() - cobranças atendem às regras de negócios definidas
+
+
+// Modificações identificadas pelos alunos
+  // --> TODO: Verificar se o código está de acordo com o diagrama de classes
+  // --> TODO: Testar a interface 
+  // --> TODO: Realizar teste de cobrança
+  // --> TODO: Organizar exibição de mensagens para o usuário (Nos aquivos que tiverem output de informações)
+  // --> TODO: Implementar método hashCode e Equals (Nos arquivos que possuirem essa mensagem no final do arquivo)
+  // --> TODO: Adicionar método getCOnsultorio() no arquivo Agenda.java
+
 class Main {
   public static void main(String[] args) {
     CadastraDados banco = new CadastraDados();
@@ -62,6 +98,5 @@ class Main {
     // Consulta exibe = new Consulta(true, paciente1, medico1);
     // System.out.println("opção 4: Emitir nota de cobrança!"+ "\n" + exibe.emitirNotaCobranca());
   }
-
-    //TODO: Testar a interface 
-    //TODO: Realizar teste de cobrança
+  // --> TODO: Implementar método hashCode e Equals
+  // --> TODO: Organizar exibição de mensagens para o usuário
