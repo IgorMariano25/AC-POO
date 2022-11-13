14cfc02c968f9eb02d8b801dcfd71da96d198c27
Igor Mariano <igor-mariano@outlook.com>
2022-11-13 18:02:06 -0300

    Criando o arquivo Gerenciador.java com modificações

---
+++ src/util/Gerenciador.java
@@ -0,0 +1,60 @@
+package util;
+
+import pessoas.Medico;
+import pessoas.Paciente;
+import entidades.Cliente;
+import entidades.Clinica;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Gerenciador {
+    private Scanner scanner;
+    public static void exibeOpcoesMenu() {
+        System.out.println( "\nSistema de agendamento de consultas");
+        System.out.println( "Informe a opção desejada:");
+        System.out.println( "1 - Cadastrar paciente");
+        System.out.println( "2 - Exibir pacientes cadastrados");
+        System.out.println( "3 - Cadastrar médico");
+        System.out.println( "4 - Cadastrar paciente");
+        System.out.println( "5 - Cadastrar clinica");
+        System.out.println( "Qualquer outra tecla - Sair do programa");
+    }
+    
+    public static String lerTexto() {
+        return scanner.nextLine();
+    }
+    public static String processaOpcoesMenu() throws IOException {
+        String opcao = Gerenciador.lerTexto();
+        String opcaoMenu = "0";
+
+        switch (opcaoMenu) {
+            case "1": //cliente
+            while (!opcaoMenu.equals("")){
+                Cliente.exibeOpcoesMenu();
+                opcaoMenu = processaOpcoesMenu();
+                    }
+
+            break;
+
+            case "2": //médicos
+            while (!opcaoMenu.equals("")){
+                Medico.exibeOpcoesMenu();
+                opcaoMenu = processaOpcoesMenu();
+                    }
+
+            case "3": //clínicas
+            while (!opcaoMenu.equals("")){
+                Clinica.exibeOpcoesMenu();
+                opcaoMenu = processaOpcoesMenu();
+                    }
+
+            default:
+                System.out.println("Volte sempre!");
+                opcao = "";
+            break;
+        }
+
+        return opcao;
+    }
+}
