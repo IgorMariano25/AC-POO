ab4583e7720d8054d08f8ba1cb4acce27d8e90f4
Igor Mariano <igor-mariano@outlook.com>
2022-11-13 18:49:16 -0300

    Alterando exibição do menu no arquivo Cliente.java

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -23,8 +23,9 @@ public class Cliente {
         System.out.println( "1 - Cadastrar paciente");
         System.out.println( "2 - Exibir pacientes cadastrados");
         System.out.println( "3 - Cadastrar médico");
-        System.out.println( "4 - Cadastrar paciente");
+        System.out.println(" 4 - Exibir Médicos cadastrados");
         System.out.println( "5 - Cadastrar clinica");
+        System.out.println(" 6 - Exibir clinica cadastrada");
         System.out.println( "Qualquer outra tecla - Sair do programa");
     }
 
