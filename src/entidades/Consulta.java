bffb86d8d5e6a5cf320b18cf1b9dacf5697f3711
Igor Mariano <igor-mariano@outlook.com>
2022-11-15 15:35:18 -0300

    Removendo método exibeOpcoesMenu() da classe Consulta

--- src/entidades/Consulta.java
+++ src/entidades/Consulta.java
@@ -17,18 +17,6 @@ public class Consulta {
         this.endereco = endereco;
     }
 
-    public static void exibeOpcoesMenu() {
-        System.out.println( "\nSistema de agendamento de consultas");
-        System.out.println( "Informe a opção desejada:");
-        System.out.println( "1 - Cadastrar paciente");
-        System.out.println( "2 - Exibir pacientes cadastrados");
-        System.out.println( "3 - Cadastrar médico");
-        System.out.println(" 4 - Exibir Médicos cadastrados");
-        System.out.println( "5 - Cadastrar clinica");
-        System.out.println(" 6 - Exibir clinica cadastrada");
-        System.out.println( "Qualquer outra tecla - Sair do programa");
-    }
-
     @Override
     public boolean equals(Object obj) {
         if (obj == this)
